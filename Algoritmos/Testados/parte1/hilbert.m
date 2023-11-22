9bd9cb859698e153f5b2f4c4048981d5eb2b72b0
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2023-11-22 16:57:38 -0300

    Update hilbert.m

--- Algoritmos/Testados/parte1/hilbert.m
+++ Algoritmos/Testados/parte1/hilbert.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -19,8 +19,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-05-26
+## Author: Breno
+## Created: 2023-11-11
 
 function [H, Hinv, NinfH, NinfHinv]  = hilbert (n)
     for i = 1 : n
