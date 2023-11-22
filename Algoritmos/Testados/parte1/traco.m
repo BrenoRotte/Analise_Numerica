05fd60edfdb2cd7ae264610c0110955921faea1e
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2023-11-22 17:01:58 -0300

    Update traco.m

--- Algoritmos/Testados/parte1/traco.m
+++ Algoritmos/Testados/parte1/traco.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -20,8 +20,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-05-23
+## Author: Breno
+## Created: 2023-11-11
 
 function soma = traco (A)
   soma = 0;
