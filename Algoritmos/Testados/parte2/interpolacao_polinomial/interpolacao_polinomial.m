a49493125fef613ba903b4d21ba0237ce7bcdc82
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2023-11-24 19:26:24 -0300

    Update interpolacao_polinomial.m

--- Algoritmos/Testados/parte2/interpolacao_polinomial/interpolacao_polinomial.m
+++ Algoritmos/Testados/parte2/interpolacao_polinomial/interpolacao_polinomial.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -22,8 +22,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-07-08
+## Author: Breno
+## Created: 2023-11-20
 
 function [Yinter, coef, Conderro] = interpolacao_polinomial(x, y, ValInter)
   if length(x) ~= length(y)
