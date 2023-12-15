d4e0c1f0e0bf5dcdf418769b1a43380e954a9487
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2023-12-15 15:51:23 -0300

    Update precisao_maquina.m

--- Algoritmos/Testados/parte2/regressao_linear_dvs/precisao_maquina.m
+++ Algoritmos/Testados/parte2/regressao_linear_dvs/precisao_maquina.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -19,8 +19,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-07-08
+## Author: Breno
+## Created: 2023-12-05
 
 function epsilon = precisao_maquina
   Eps1 = 1;
