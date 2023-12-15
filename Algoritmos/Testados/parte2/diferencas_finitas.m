dbaa00f9ee845252d3185d76df54a80cac3ae202
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2023-12-15 15:57:33 -0300

    Update diferencas_finitas.m

--- Algoritmos/Testados/parte2/diferencas_finitas.m
+++ Algoritmos/Testados/parte2/diferencas_finitas.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -21,8 +21,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-07-19
+## Author: Breno
+## Created: 2023-12-05
 
 function T = diferencas_finitas (x, y)
   m = length(x);
