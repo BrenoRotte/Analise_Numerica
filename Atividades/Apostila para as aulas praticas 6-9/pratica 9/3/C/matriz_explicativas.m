48c58088839b0b9810982e323b8cf9e58d5cb80f
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2023-12-18 19:29:21 -0300

    Update matriz_explicativas.m

--- Atividades/Apostila para as aulas praticas 6-9/pratica 9/3/C/matriz_explicativas.m
+++ Atividades/Apostila para as aulas praticas 6-9/pratica 9/3/C/matriz_explicativas.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
+## Author: Breno
 ## Created: 2023-07-05
 
 function [MatX, Info] = matriz_explicativas (n, v, p, ii, x)
