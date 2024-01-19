52e94612e94225e05b5128f9577d97f2fb99659a
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-01-19 16:09:11 -0300

    Update coeficientes_cotes.m

--- Algoritmos/Testados/parte3/Integral/newton_cotes/coeficientes_cotes.m
+++ Algoritmos/Testados/parte3/Integral/newton_cotes/coeficientes_cotes.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -28,8 +28,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-07-20
+## Author: Breno
+## Created: 2023-01-18
 
 function [Den, VetC, Info] = coeficientes_cotes (n)
   if n < 1 || n > 10
