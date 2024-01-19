92afd407b00e052dcf9227d3746f7ed13235e583
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-01-19 16:51:09 -0300

    Update abm4.m

--- Atividades/Apostila para as aulas praticas 10-14/14/9/abm4.m
+++ Atividades/Apostila para as aulas praticas 10-14/14/9/abm4.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -21,8 +21,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-08-08
+## Author: Breno
+## Created: 2024-01-18
 
 function [VetX, VetY, Erro] = abm4 (a, b, y0, m, f)
   nc = 3; # numero de correcoes
