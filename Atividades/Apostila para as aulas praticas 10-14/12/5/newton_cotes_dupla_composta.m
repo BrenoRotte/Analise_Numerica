9fec65b6c216fdccfa09356d2c7553c7c46c870d
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-01-19 16:44:12 -0300

    Update newton_cotes_dupla_composta.m

--- Atividades/Apostila para as aulas praticas 10-14/12/5/newton_cotes_dupla_composta.m
+++ Atividades/Apostila para as aulas praticas 10-14/12/5/newton_cotes_dupla_composta.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 dgsan
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -33,8 +33,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: dgsan <dgsan@LAPTOP-3MAB8DDK>
-## Created: 2023-08-16
+## Author: Breno
+## Created: 2024-01-18
 
 function [int,info] = newton_cotes_dupla_composta (ax,bx,nx,mx,ay,by,ny,my,f)
   if nx == 1 && ny == 1
