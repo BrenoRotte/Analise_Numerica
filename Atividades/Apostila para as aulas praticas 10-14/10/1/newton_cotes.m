d3a87e6da7ff5a561545df1102c8a6d03b2eed5e
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-01-19 16:34:56 -0300

    Update newton_cotes.m

--- Atividades/Apostila para as aulas praticas 10-14/10/1/newton_cotes.m
+++ Atividades/Apostila para as aulas praticas 10-14/10/1/newton_cotes.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -32,8 +32,9 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-07-24
+## Author: Breno
+## Created: 2024-01-18
+
 
 function [Integral, Info] = newton_cotes (a, b, n, m, f)
   Integral = 0;
