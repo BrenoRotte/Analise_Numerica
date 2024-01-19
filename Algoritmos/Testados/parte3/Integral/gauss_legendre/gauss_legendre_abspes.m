54f8cb43a833e626ab73c56754f96236b2d602d3
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-01-19 16:07:10 -0300

    Update gauss_legendre_abspes.m

--- Algoritmos/Testados/parte3/Integral/gauss_legendre/gauss_legendre_abspes.m
+++ Algoritmos/Testados/parte3/Integral/gauss_legendre/gauss_legendre_abspes.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -24,8 +24,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-07-23
+## Author: Breno
+## Created: 2023-01-18
 
 # Nao sei o que e ingama
 
