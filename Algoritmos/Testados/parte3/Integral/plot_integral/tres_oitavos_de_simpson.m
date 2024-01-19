81f5cc915f0b5ec64c6111c66615c2921dde038b
Breno Rotte <114703393+BrenoRotte@users.noreply.github.com>
2024-01-19 16:18:51 -0300

    Update tres_oitavos_de_simpson.m

--- Algoritmos/Testados/parte3/Integral/plot_integral/tres_oitavos_de_simpson.m
+++ Algoritmos/Testados/parte3/Integral/plot_integral/tres_oitavos_de_simpson.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2023 Diego Sanches
+## Copyright (C) 2023 Breno
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -23,8 +23,8 @@
 ## @seealso{}
 ## @end deftypefn
 
-## Author: Diego Sanches
-## Created: 2023-08-01
+## Author: Breno
+## Created: 2024-01-18
 
 function Integral = tres_oitavos_de_simpson (a, b, n, f)
   h = (b-a)/(3*n);
